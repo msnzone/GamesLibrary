727029cc8d25626bbdf42c571b46a63e1e41d841
solitareonline <tonygau.118@gmail.com>
2025-12-22 18:47:58 +0700

    update actice usse

--- G1/cookie-clicker/main.js
+++ G1/cookie-clicker/main.js
@@ -1174,7 +1174,7 @@ Game.Launch=function()
 		l('javascriptError').innerHTML=
 		'<div class="title">Oops. Wrong address!</div>'+
 		'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
-		'You can <a href="//orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
+		'You can <a href="//https://msngames.io/cookie-clicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
 		'<small>(If for any reason, you are unable to access the game on the official URL, we are currently working on a second domain.)</small></div>';
 	}
 	Game.timedout=false;
@@ -14439,25 +14439,41 @@ Game.Launch();
 //try {Game.Launch();}
 //catch(err) {console.log('ERROR : '+err.message);}
 
+// window.onload=function()
+// {
+	
+// 	if (!Game.ready)
+// 	{
+// 		if (top!=self) Game.ErrorFrame();
+// 		else
+// 		{
+// 			console.log('[=== '+choose([
+// 				'Oh, hello!',
+// 				'hey, how\'s it hangin',
+// 				'3kh0 is the best lol',
+// 				'About to cheat in some cookies or just checking for bugs?',
+// 				'Remember : cheated cookies taste awful!',
+// 				'Hey, Orteil here. Cheated cookies taste awful... or do they?',
+// 			])+' ===]');
+// 			Game.Load();
+// 			//try {Game.Load();}
+// 			//catch(err) {console.log('ERROR : '+err.message);}
+// 		}
+// 	}
+// };
+
 window.onload=function()
 {
 	
 	if (!Game.ready)
-	{
-		if (top!=self) Game.ErrorFrame();
-		else
-		{
-			console.log('[=== '+choose([
-				'Oh, hello!',
-				'hey, how\'s it hangin',
-				'3kh0 is the best lol',
-				'About to cheat in some cookies or just checking for bugs?',
-				'Remember : cheated cookies taste awful!',
-				'Hey, Orteil here. Cheated cookies taste awful... or do they?',
-			])+' ===]');
-			Game.Load();
-			//try {Game.Load();}
-			//catch(err) {console.log('ERROR : '+err.message);}
-		}
+	{		
+		console.log('[=== '+choose([
+			'Oh, hello!',
+			'hey, how\'s it hangin',			
+			'About to cheat in some cookies or just checking for bugs?',
+			'Remember : cheated cookies taste awful!',
+			'Hey, Orteil here. Cheated cookies taste awful... or do they?',
+		])+' ===]');
+		Game.Load();		
 	}
 };
